e62bb8b5da8978f7cbabcade267fd1ac9f10a045
Liliya <76985102+Liliya2727@users.noreply.github.com>
2025-02-23 09:35:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,21 +8,21 @@ REPLACE="
 "
 sleep 2
 ui_print
-ui_print "ZEPASSCHARGE!"
+ui_print "ANTARES!"
 ui_print 
 ui_print "by : @Zexshia!"
 ui_print
-sleep 0.2
+sleep 1
 ui_print "Device : $(getprop ro.product.board) "
 
 sleep 2
 
 ui_print
 ui_print "- Extracting module files"
-mkdir /data/ZepassCharge
+mkdir /data/Antares
 unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 unzip -o "$ZIPFILE" 'service.sh' -d "$MODPATH" >&2
-unzip -o "$ZIPFILE" 'gamelist.txt' -d "/data/ZepassCharge" >&2
+unzip -o "$ZIPFILE" 'gamelist.txt' -d "/data/Antares" >&2
 
 ui_print "- Installing bellavita toast"
 unzip -o "$ZIPFILE" 'toast.apk' -d $MODPATH >&2
