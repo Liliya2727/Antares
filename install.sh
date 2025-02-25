a2e4fc1e1b3392e60542d2f318f5d966450a8e29
Liliya <76985102+Liliya2727@users.noreply.github.com>
2025-02-25 18:07:03 +0700

    update installer, and remove useless line in service.sh, added debug thermal throttle suppor

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 SKIPMOUNT=false
 PROPFILE=true
 POSTFSDATA=false
@@ -6,21 +7,25 @@ LATESTARTSERVICE=true
 REPLACE="
 
 "
+
 sleep 2
 ui_print
 ui_print
 ui_print "          ANTARES!            "
 ui_print 
 ui_print
-ui_print "- Releases : 24/02/2025"
+ui_print "- Releases : 26/02/2025"
 ui_print "- Author : @Zexshia"
-ui_print "- Version : 1.2"
+ui_print "- Version : 1.5"
 sleep 1
 ui_print "- Device : $(getprop ro.product.board) "
 sleep 2
 ui_print "- Installing Antares!"
 sleep 2
-ui_print "- Extracting module files"
+ui_print "- Extracting module files.."
+sleep 2
+ui_print "- Extracting system.prop"
+sleep 1
 mkdir /data/Antares
 unzip -o "$ZIPFILE" 'common/system.prop' -d $MODPATH >&2
 unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
--- service.sh
+++ service.sh
@@ -11,84 +11,30 @@ apply() {
 sleep 1
 # Renderer Configurations
 resetprop -n debug.hwui.renderer skiagl
+resetprop -n -v debug.thermal.throttle.support no
 resetprop -n vendor.debug.renderengine.backend skiaglthreaded
 resetprop -n debug.renderengine.backend skiaglthreaded
 
-# Ged Optimisation
-echo "1" > /sys/module/ged/parameters/gx_game_mode
-echo "1" > /sys/module/ged/parameters/ged_smart_boost
-echo "1" > /sys/module/ged/parameters/cpu_boost_policy
-echo "1" > /sys/module/ged/parameters/boost_extra
-echo "886000" > /sys/module/ged/parameters/gpu_cust_upbound_freq
-echo "1" > /sys/module/ged/parameters/enable_gpu_boost
-echo "1" > /sys/module/ged/parameters/ged_boost_enable
-echo "1" > /sys/module/ged/parameters/gx_boost_on
-echo "1" > /sys/module/ged/parameters/boost_gpu_enable
-echo "1" > /sys/module/ged/parameters/ged_monitor_3D_fence_systrace
-echo "90" > /sys/module/ged/parameters/g_fb_dvfs_threshold
-echo "1" > /sys/module/ged/parameters/g_gpu_timer_based_emu
-echo "1" > /sys/module/ged/parameters/gpu_cust_boost_freq
-echo "1" > /sys/kernel/ged/hal/gpu_boost_level
-
-# Volt Optimizer
-echo "-14" > /proc/eem/EEM_DET_B/eem_offset
-echo "-14" > /proc/eem/EEM_DET_BL/eem_offset
-echo "-14" > /proc/eem/EEM_DET_L/eem_offset
-echo "-9" > /proc/eem/EEM_DET_CCI/eem_offset
-echo "-2" > /proc/eemg/EEMG_DET_GPU/eemg_offset
-echo "-2" > /proc/eemg/EEMG_DET_GPU_HI/eemg_offset
-
-# Schedul Optimisation
-echo "500000" > /proc/sys/kernel/sched_migration_cost_ns
-echo "30" > /proc/sys/kernel/perf_cpu_time_max_percent
-echo "3" > /proc/sys/vm/drop_caches
-echo "256" > /proc/sys/kernel/sched_nr_migrate
-echo "10000000" > /proc/sys/kernel/sched_latency_ns
-echo "1024" > /proc/sys/kernel/sched_util_clamp_max
-echo "1024" > /proc/sys/kernel/sched_util_clamp_min
-echo "1" > /proc/sys/kernel/sched_child_runs_first
-echo "1" > /proc/sys/kernel/sched_tunable_scaling
-echo "0" > /proc/sys/kernel/sched_energy_aware
-
-# mmcblk0
-echo "0" > /sys/block/mmcblk0/queue/add_random
-echo "0" > /sys/block/mmcblk0/queue/iostats
-echo "0" > /sys/block/mmcblk0/queue/nomerges
-echo "0" > /sys/block/mmcblk0/queue/rotational
-echo "2" > /sys/block/mmcblk0/queue/rq_affinity
-echo "256" > /sys/block/mmcblk0/queue/nr_requests
-echo "2048" > /sys/block/mmcblk0/queue/read_ahead_kb
-
-# Sda
-echo "0" > /sys/block/sda/queue/add_random
-echo "0" > /sys/block/sda/queue/iostats
-echo "0" > /sys/block/sda/queue/nomerges
-echo "0" > /sys/block/sda/queue/rotational
-echo "2" > /sys/block/sda/queue/rq_affinity
-echo "256" > /sys/block/sda/queue/nr_requests
-echo "2048" > /sys/block/sda/queue/read_ahead_kb
-
-# Sdb
-echo "0" > /sys/block/sdb/queue/add_random
-echo "0" > /sys/block/sdb/queue/iostats
-echo "0" > /sys/block/sdb/queue/nomerges
-echo "0" > /sys/block/sdb/queue/rotational
-echo "2" > /sys/block/sdb/queue/rq_affinity
-echo "256" > /sys/block/sdb/queue/nr_requests
-echo "2048" > /sys/block/sdb/queue/read_ahead_kb
-
-# Sdc
-echo "0" > /sys/block/sdc/queue/add_random
-echo "0" > /sys/block/sdc/queue/iostats
-echo "0" > /sys/block/sdc/queue/nomerges
-echo "0" > /sys/block/sdc/queue/rotational
-echo "2" > /sys/block/sdc/queue/rq_affinity
-echo "256" > /sys/block/sdc/queue/nr_requests
-echo "2048" > /sys/block/sdc/queue/read_ahead_kb
-
-# SET FREQ VOLTAGE
-echo "6000000" > /sys/class/power_supply/battery/voltage_now
-echo "1" > /sys/devices/system/cpu/perf/enable
+# Params Executions
+resetprop -n -v ro.surface_flinger.max_frame_buffer_acquired_buffers=5
+resetprop -n -v debug.sf.early_phase_offset_ns=500000
+resetprop -n -v debug.sf.early_app_phase_offset_ns=500000
+resetprop -n -v debug.sf.early_gl_phase_offset_ns=1000000
+resetprop -n -v debug.sf.early_gl_app_phase_offset_ns=1000000
+resetprop -n -v debug.sf.high_fps_early_phase_offset_ns=1000000
+resetprop -n -v debug.sf.high_fps_early_gl_phase_offset_ns=1000000
+resetprop -n -v debug.sf.high_fps_late_app_phase_offset_ns=1000000
+resetprop -n -v debug.sf.phase_offset_threshold_for_next_vsync_ns=1000000
+resetprop -n -v persist.sys.NV_FPSLIMIT=0
+resetprop -n -v persist.sys.NV_POWERMODE=3
+resetprop -n -v persist.sys.NV_PROFVER=15
+resetprop -n -v persist.sys.NV_STEREOCTRL=0
+resetprop -n -v persist.sys.NV_STEREOSEPCHG=0
+resetprop -n -v persist.sys.NV_STEREOSEP=50
+resetprop -n -v debug.sf.showupdates=0 
+resetprop -n -v debug.sf.showcpu=0 
+resetprop -n -v debug.sf.showbackground=0 
+resetprop -n -v debug.sf.showfps=0
 
 # TURN OFF ALL POLICIES EXCEPT PPM_POLICY_HARD_USER_LIMIT
 apply "0 0" /proc/ppm/policy_status  # PPM_POLICY_PTPOD
@@ -101,21 +47,6 @@ apply "6 1" /proc/ppm/policy_status  # PPM_POLICY_HARD_USER_LIMIT
 apply "7 0" /proc/ppm/policy_status  # PPM_POLICY_USER_LIMIT
 apply "8 0" /proc/ppm/policy_status  # PPM_POLICY_LCM_OFF
 apply "9 0" /proc/ppm/policy_status  # PPM_POLICY_SYS_BOOST
-
-# Scheduler IO
-echo "mq-deadline" > /sys/block/mmcblk0/queue/scheduler
-echo "deadline" > /sys/block/sda/queue/scheduler
-echo "deadline" > /sys/block/sdb/queue/scheduler
-echo "deadline" > /sys/block/sdc/queue/scheduler
-echo "deadline" > /sys/block/dm-0/queue/scheduler
-
-# Schedhorizon Tunes
-echo "50 100 200 400 600" > /sys/devices/system/cpu/cpufreq/policy0/schedhorizon/up_delay
-echo "500000 850000 1250000 1450000 1800000" > /sys/devices/system/cpu/cpufreq/policy0/schedhorizon/efficient_freq
-echo "50 100 200 400 600 800" > /sys/devices/system/cpu/cpufreq/policy4/schedhorizon/up_delay
-echo "437000 902000 1162000 1451000 1740000 1985000" > /sys/devices/system/cpu/cpufreq/policy4/schedhorizon/efficient_freq
-echo "30 50 150 300 500 700" > /sys/devices/system/cpu/cpufreq/policy7/schedhorizon/up_delay
-echo "659000 1108000 1370000 1632000 1820000 2284000" > /sys/devices/system/cpu/cpufreq/policy7/schedhorizon/efficient_freq
 
 # Run Antares Service
 Antares >/dev/null 2>&1
