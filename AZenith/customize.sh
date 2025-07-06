bb4726d1a90237c37ff05b0e5fb1db02ba98d20d
Liliya <76985102+Liliya2727@users.noreply.github.com>
2025-07-06 11:13:26 +0700

    Add files via upload

--- AZenith/customize.sh
+++ AZenith/customize.sh
@@ -16,116 +16,114 @@
 # limitations under the License.
 #
 
+
 SKIPMOUNT=false
 PROPFILE=false
 POSTFSDATA=false
 LATESTARTSERVICE=true
 
-# Get module version and author from module.prop
+# Paths
+MODULE_CONFIG="/data/adb/.config/AZenith"
 MODVER=$(grep "^version=" "$MODPATH/module.prop" | cut -d '=' -f2)
 AUTHOR=$(grep "^author=" "$MODPATH/module.prop" | cut -d '=' -f2)
 device_codename=$(getprop ro.product.board)
 chip=$(getprop ro.hardware)
 
-MODULE_CONFIG="/data/adb/.config/AZenith"
+# Create File
 make_node() {
-	[ ! -f "$2" ] && echo "$1" >"$2"
+    [ ! -f "$2" ] && echo "$1" > "$2"
 }
 
-# Display banner
+
+# Displaybanner
 ui_print ""
 ui_print "              AZenith              "
 ui_print ""
-ui_print "- Release Date : 01/07/2025"
-ui_print "- Author        : ${AUTHOR}"
-ui_print "- Version       : ${MODVER}"
-ui_print "- Device        : $(getprop ro.product.board)"
+ui_print "- Release Date : 05/07/2025"
+ui_print "- Author       : ${AUTHOR}"
+ui_print "- Version      : ${MODVER}"
+ui_print "- Device       : ${device_codename}"
 ui_print "- Installing AZenith..."
 
-# Extracting module files
+# Extract Module Directiories
 mkdir -p "$MODULE_CONFIG"
+
+# Extract Module files
 ui_print "- Extracting system directories..."
 extract -o "$ZIPFILE" 'system/*' -d "$MODPATH" >&2
-ui_print "- Extracting libs directories..."
-unzip -o "$ZIPFILE" 'service.sh' -d "$MODPATH" >&2
-ui_print "- Extracting gamelist..."
-unzip -qo "$ZIPFILE" 'gamelist.txt' -d "$MODULE_CONFIG" >&2
-ui_print "- Extracting module Icon..."
-unzip -qo "$ZIPFILE" 'AZenith_icon.png' -d "/data/local/tmp" >&2
+unzip -qo "$ZIPFILE" service.sh -d "$MODPATH" >&2
+unzip -qo "$ZIPFILE" gamelist.txt -d "$MODULE_CONFIG" >&2
+unzip -qo "$ZIPFILE" AZenith_icon.png -d /data/local/tmp >&2
 
-checkpath() {
-    if [ -e "$1" ]; then
-        return 0 
-    fi
-    return 1  
-}
+# Apply Tweaks Based on Chipset
+chipset=$(grep -i 'hardware' /proc/cpuinfo | uniq | cut -d ':' -f2 | sed 's/^[ \t]*//')
+[ -z "$chipset" ] && chipset="$(getprop ro.board.platform) $(getprop ro.hardware)"
 
-# List of paths
-bypasslist="
-/sys/devices/platform/charger/bypass_charger
-/sys/devices/platform/charger/tran_aichg_disable_charger
-/proc/mtk_battery_cmd/current_cmd
-/sys/devices/platform/mt-battery/disable_charger
-"
-# Checking bypass
-ui_print "- Checking Bypass compatibility..."
-bypasspath=""
-for path in $bypasslist; do
-    if checkpath "$path"; then
-        bypasspath="$path"
-        break 
-    fi
-done
+case "$chipset" in
+    *mt* | *MT* | *mediatek* | *Mediatek*)
+        soc="MediaTek"
+        ui_print "- Applying Tweaks for MediaTek $chipset"
+        make_node 1 "$MODULE_CONFIG/soctype"
+        
+        ;;
+    *sdm* | *SM* | *sm* | *Snapdragon* | *qcom* | *QCOM*)
+        soc="Snapdragon"
+        ui_print "- Applying Tweak for Snapdragon $chipset"
+        make_node 2 "$MODULE_CONFIG/soctype"
+        
+        ;;
+    *)
+        soc="Unknown"
+        ui_print "- Unsupported Chipset: $chipset, Skipping some tweaks"
+        make_node 0 "$MODULE_CONFIG/soctype"
+        
+        ;;
+esac
+# Soc Type
+# 1) MediaTek
+# 2) Snapdragon 
+# 0) Unknown/No Unsupported
+# /// More chipset will be added soon ////
 
-# Fallback to nb html if bypass unsupported
-if [ -z "$bypasspath" ]; then
-    ui_print "- Inflating WebUi"
-    mv "$MODPATH/assets/include/indexnbhtml" "$MODPATH/webroot/index.html"
-    mv "$MODPATH/assets/include/fn762agnbjs" "$MODPATH/webroot/fn762ag.js"
-    else
-    ui_print "- Inflating WebUi"
-    ui_print "- path: $path"
-    mv "$MODPATH/webroot/include/indexbphtml" "$MODPATH/webroot/assets/index.html"
-    mv "$MODPATH/assets/include/fn762agbpjs" "$MODPATH/webroot/assets/fn762ag.js"
-fi
+# Inflate WebUI Based on SoC
+case "$soc" in
+    "MediaTek")
+        ui_print "- Inflating WebUI for MediaTek"
+        mv "$MODPATH/webroot/include/mtkhtml" "$MODPATH/webroot/assets/index.html"
+        mv "$MODPATH/assets/include/mtkjs" "$MODPATH/webroot/assets/fn762ag.js"
+        ;;
+    "Snapdragon")
+        ui_print "- Inflating WebUI for Snapdragon"
+        mv "$MODPATH/assets/include/sdmhtml" "$MODPATH/webroot/index.html"
+        mv "$MODPATH/assets/include/sdmjs" "$MODPATH/webroot/fn762ag.js"
+        ;;
+    *)
+        ui_print "- Inflating Universal WebUI"
+        mv "$MODPATH/assets/include/bschtml" "$MODPATH/webroot/index.html"
+        mv "$MODPATH/assets/include/bscjs" "$MODPATH/webroot/fn762ag.js"
+        ;;
+esac
+# I made 3 differs WebUI for this to match every Chipsets
+# # # # # # 
 
-# Installing toast
+# Install toast if not installed
 if pm list packages | grep -q bellavita.toast; then
     ui_print "- Bellavita Toast is already installed."
 else
     ui_print "- Extracting Bellavita Toast..."
-    unzip -o "$ZIPFILE" 'toast.apk' -d "$MODPATH" >&2
+    unzip -qo "$ZIPFILE" toast.apk -d "$MODPATH" >&2
     ui_print "- Installing Bellavita Toast..."
     pm install "$MODPATH/toast.apk"
     rm "$MODPATH/toast.apk"
 fi
 
-# Check device chipset
-chipset=$(grep "Hardware" /proc/cpuinfo | uniq | cut -d ':' -f 2 | sed 's/^[ \t]*//')
-[ -z "$chipset" ] && chipset="$(getprop ro.board.platform) $(getprop ro.hardware)"
-case "$chipset" in
-    *mt* | *MT*) 
-        soc=1
-        ui_print "- Implementing tweaks for MediaTek $chip"
-        
-        ;;
-    *)  
-        ui_print "! Unsupported chipset detected: $chipset"
-        ui_print "! This is Only for MediaTek."
-        abort
-        ;;
-esac
 
-# Move Important File to run Azenith 
-mv "$MODPATH/assets/availableFreq" "/data/adb/.config/AZenith/availableFreq"
-mv "$MODPATH/assets/customFreqOffset" "/data/adb/.config/AZenith/customFreqOffset"
-
+# Make module config 
 make_node 0 "$MODULE_CONFIG/clearbg"
 make_node 0 "$MODULE_CONFIG/bypass"
 make_node 0 "$MODULE_CONFIG/APreload"
 make_node 0 "$MODULE_CONFIG/logger"
 make_node 0 "$MODULE_CONFIG/logd"
-make_node 0 "$MODULE_CONFIG/FSTrim"
 make_node 0 "$MODULE_CONFIG/DThermal"
 make_node 0 "$MODULE_CONFIG/dnd"
 make_node 0 "$MODULE_CONFIG/schedtunes"
@@ -135,20 +133,24 @@ make_node 0 "$MODULE_CONFIG/SFL"
 make_node 0 "$MODULE_CONFIG/malisched"
 make_node 0 "$MODULE_CONFIG/cpulimit"
 make_node "1000 1000 1000 1000" "$MODULE_CONFIG/color_scheme"
-make_node "OFF 90% 80% 70% 60% 50% 40%" "$MODULE_CONFIG/availableFreq"
-make_node "OFF" "$MODULE_CONFIG/customFreqOffset"
+make_node "Disabled 90% 80% 70% 60% 50% 40%" "$MODULE_CONFIG/availableFreq"
+make_node "Disabled" "$MODULE_CONFIG/customFreqOffset"
+make_node "Disabled 60hz 90hz 120hz" "$MODULE_CONFIG/availableValue"
+make_node "Disabled" "$MODULE_CONFIG/customVsync"
 
-# Cleanup file
+# Make sure to enable Auto Every installment and Update
+echo 1 > "$MODULE_CONFIG/AIenabled"
+
+# Remove useless files
 ui_print "- Cleaning up files..."
-rm -rf "$MODPATH/assets"
-rm -rf "$MODPATH/gamelist.txt"
-rm -rf "$MODPATH/toast.apk"
+rm -rf "$MODPATH/webroot/include" "$MODPATH/gamelist.txt" "$MODPATH/toast.apk"
 
-# Final permission setup
+# Set Permissions
 ui_print "- Setting Permissions..."
 set_perm_recursive "$MODPATH/system/bin" 0 2000 0777 0777
-chmod +x "$MODPATH/system/bin/AZenith" 
-chmod +x "$MODPATH/system/bin/AZenith_Profiler" 
-chmod +x "$MODPATH/system/bin/bypassCharge" 
-chmod +x "$MODPATH/system/bin/vmt" 
+chmod +x "$MODPATH/system/bin/AZenith"
+chmod +x "$MODPATH/system/bin/AZenith_Profiler"
+chmod +x "$MODPATH/system/bin/bypassCharge"
+chmod +x "$MODPATH/system/bin/vmt"
+
 ui_print "- Installation complete!"
