c6b04363a0d42ac6ac3edf4fe3000487f6cf3f78
Liliya <syarifbintang16@gmail.com>
2025-08-23 01:57:53 +0700

    Update uninstall.sh

--- mainfiles/uninstall.sh
+++ mainfiles/uninstall.sh
@@ -19,9 +19,9 @@
 # Remove Persistent Properties
 props="persist.sys.azenith.state \
 persist.sys.azenith.debugmode \
-persist.sys.azenith.justintime \
+persist.sys.azenithconf.justintime \
 persist.sys.azenith.service \
-persist.sys.azenith.disabletrace \
+persist.sys.azenithconf.disabletrace \
 persist.sys.azenithconf.logd \
 persist.sys.azenithconf.DThermal \
 persist.sys.azenithconf.SFL \
