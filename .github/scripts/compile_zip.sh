216c0fc5911331a4899ba62568ccafb8af663f20
Liliya <syarifbintang16@gmail.com>
2025-08-24 22:06:52 +0700

    Test Upload Using Bot

--- .github/scripts/compile_zip.sh
+++ .github/scripts/compile_zip.sh
@@ -45,7 +45,7 @@ cp LICENSE ./mainfiles
 find mainfiles/system/bin -maxdepth 1 -type f -name "*.sh" -exec sh -c 'mv -- "$0" "${0%.sh}"' {} \;
 
 # Parse version info to module prop
-zipName="AZenith-$version-$release_code"
+zipName="AZenith-$version-$release_code.zip"
 echo "zipName=$zipName" >>"$GITHUB_OUTPUT"
 
 # Generate sha256sum for integrity checkup
--- .github/scripts/telebot.sh
+++ .github/scripts/telebot.sh
@@ -16,20 +16,11 @@ if [ ! -f "$file" ]; then
     echo "error: File not found" >&2
     exit 1
 fi
-filename=$(basename -- "$file")
-safe_name="${filename/-release/.rel}"
 
-if [ "$safe_name" != "$filename" ]; then
-    cp "$file" "/tmp/$safe_name"
-    file="/tmp/$safe_name"
-    filename="$safe_name"
-fi
-
-curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
-    -F "chat_id=$CHAT_ID" \
-    -F "document=@$file;filename=$filename" \
-    -F "thumb=@$thumbnail" \
-    -F "caption=$msg" \
-    -F "parse_mode=MarkdownV2" \
-    -F "disable_web_page_preview=true"
-    
+curl -s "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
+	-F document=@"$file" \
+	-F chat_id="$CHAT_ID" \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=markdownv2" \
+	-F thumb=@"$thumbnail" \
+	-F caption="$msg"
