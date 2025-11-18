49f1503049f4216975bd4d8abca587512e05bb5b
Liliya2727 <syarifbintang16@gmail.com>
2025-11-18 15:53:41 +0700

    remove java obfuscator

--- webui/vite.config.js
+++ webui/vite.config.js
@@ -2,24 +2,12 @@ import { defineConfig } from "vite"
 import mkcert from "vite-plugin-mkcert";
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import { ViteMinifyPlugin } from 'vite-plugin-minify'
-import obfuscator from "vite-plugin-javascript-obfuscator";
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 export default defineConfig({
   root: './src',
   plugins: [
     ViteMinifyPlugin({}),
     mkcert(),
-    obfuscator({
-      exclude: [
-        'src/locales/strings/*',
-        'src/scripts/language.js',
-      ],
-      compact: true,
-      stringArray: true,
-      stringArrayThreshold: 0.75,
-      controlFlowFlattening: false,
-      deadCodeInjection: false,
-    })
   ],
   server : {
     https: true,
